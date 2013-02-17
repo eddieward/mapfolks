889ab6938a2a205d4fff681614764bc95550f2d9
eddieward <eddie@isle3.com>
2013-02-17 14:36:51 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapfolks::Application.config.secret_token = 'ace53589038338e6501508a306b38c1710dbf7259d31874301f5d157227c2a705342e5e70f5bbc36bdc380eff361e98eb3181f92775c7f4e4b52dc9b5750c53e'
